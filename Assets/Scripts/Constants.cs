c7b8f01ded0ccfae3368f873ec0dab5ab1d7ec31
Jean Alarcon <Renzy.Alarcon@hotmail.com>
2020-09-30 02:19:11 -0400

    - Added max booster count constant

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿fousing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,7 @@ using UnityEngine;
 public static class Constants
 {
     public const int maxObstacles = 5;
+    public const int maxBoosterCount = 3;
     public const float metre = 1.0f;
     public const float kilometre = metre * 1000.0f;
     public const float distanceToJapan = 68.21f * kilometre;
